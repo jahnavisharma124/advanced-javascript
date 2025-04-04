e9a4720e0e14741f425ff2aa4b6bd252df58f059
jahnavisharma124 <rekhasharma210682@gmail.com>
2025-04-04 12:52:47 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,59 @@
+// function sayHello() {
+//     let you = prompt('What is your name?');
+//    console.log('Hello', you + '!');
+// }
+// sayHello();
+
+// let varContainingFunction = function() {
+//     let varInFunction = "I'm a variable in a function";
+//     console.log("hi There!",varInFunction);
+// };
+
+// varContainingFunction();
+
+// function tester(para1, para2) {
+//     return para1 + " " + para2;
+// }
+// const test = tester("Hello", "World!");
+// console.log(test);
+
+// function add(a, b) {
+//     console.log(a+b);
+// }
+// parseInt(prompt("Enter first number: ", function(a)) {
+//     parseInt(prompt("Enter second number: ", function(b))) {
+//         add(a, b);
+//     }; 
+// }
+
+// function tester(para1, para2) {
+//     return para1 + " " + para2;
+// }
+// const test = tester("Hello", "World!");
+// console.log(test);
+// function tester(para1,para2){
+//     return para1+" "+para2;
+// }
+// const arg1="argument1";
+// const arg2="argument2";
+// console.log(tester(arg1,arg2));
+// function addNumbers(num1,num2){
+//     return parseInt(num1)+parseInt(num2);
+// }
+// const result=addNumbers("10","20");
+// console.log(result);
+// }
+// let doingStuff=(x)=>{
+//     console.log(x)
+// }
+// doingStuff("hello world")
+// doingStuff(123)
+// doingStuff("12345")
+// const arr=["squirrel","alpaca","buddy"];
+// arr.forEach(e=>console.log(e));
+let spread=["so","much","fun"];
+let message=["JavaScript","is",...spread,"and","very","powerful"];
+let message1=["JavaScript","is",spread,"and","very","powerful"];
+console.log(message);
+console.log(message1);
+
